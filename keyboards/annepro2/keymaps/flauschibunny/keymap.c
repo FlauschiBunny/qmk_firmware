fc8634e0a470d00a9ee0dfce3cca9b26770ebce8
Flauschibunny <flauschibunny@outlook.com>
2021-11-27 13:20:40 +0100

    Mouse and Numpad layers

--- keyboards/annepro2/keymaps/flauschibunny/keymap.c
+++ keyboards/annepro2/keymaps/flauschibunny/keymap.c
@@ -11,6 +11,10 @@ enum anne_pro_layers {
   _FN4_LAYER, // Dark Table or mouse in planning...
 };
 
+enum custom_keycodes { //Use USER 00 instead of SAFE_RANGE for Via. VIA json must include the custom keycode.
+	  ATABF = SAFE_RANGE, //Alt tab forwards
+	  ATABR
+	};
 // Key symbols are based on QMK. Use them to remap your keyboard
 /*
 * Layer _BASE_LAYER
@@ -54,42 +58,42 @@ enum anne_pro_layers {
   * |-----------------------------------------------------------------------------------------+
   * | Tab    |PRVAPP|NXTAPP|lst_DSK|NXT_DSK|WWW_SRCH|PGUP|HOME|UP|END | INSRT|V-UP|V-DWN|MUTE |
   * |-----------------------------------------------------------------------------------------+
-  * |        | ALT | WIN |CTRL | SHFT |WWW_BAKC|PGDWN|LEFT|DOWN|RIGHT| MSUP|MSDWN |   Enter   |
+  * |        | ALT | WIN |CTRL | SHFT |WWW_BAKC|PGDWN|LEFT|DOWN|RIGHT| BCKSPC| CALC | Enter   |
   * |-----------------------------------------------------------------------------------------+
-  * | MARK_ALL|UNDO | CUT| COPY | PASTE | REDO|  DEL  |  MPRV  | MNXT |PRINT| SCRLCK| ShiFT   |
+  * | MARK_ALL|UNDO | CUT| COPY | PASTE | REDO|  DEL  |  MPRV  | MNXT |PRINT| MAIL  | ShiFT   |
   * |-----------------------------------------------------------------------------------------+
-  * | Ctrl  |  L1   |  Alt  |            PLAY/PAUSE           |  AltGr  |      |       |      |
+  * | Ctrl  |  L1   |  Alt  |            SPACE                |  AltGr|TG(FN2)|TG(FN3)|TG(FN4)|
   * \-----------------------------------------------------------------------------------------/
   *
   */
  [_FN1_LAYER] = KEYMAP( /* Base */
     KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, TO(_BASE_LAYER),
-    KC_TRNS, LSA(KC_TAB), A(KC_TAB), C(G(KC_LEFT)), C(G(KC_RGHT)), KC_WWW_SEARCH, KC_PGUP, KC_HOME, KC_UP, KC_END, KC_INS, KC_VOLU, KC_VOLD, KC_MUTE,
-    KC_TRNS, KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT, KC_WWW_BACK, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_WH_U, KC_WH_D, KC_TRNS,
-    C(KC_A), C(KC_Z), C(KC_X), C(KC_C), C(KC_V), C(KC_Y), KC_DEL, KC_MPRV, KC_MNXT, KC_PSCREEN, KC_SCROLLLOCK, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_TRNS, ATABR, ATABF, C(G(KC_LEFT)), C(G(KC_RGHT)), KC_WWW_SEARCH, KC_PGUP, KC_HOME, KC_UP, KC_END, KC_INS, KC_VOLU, KC_VOLD, KC_MUTE,
+    KC_TRNS, KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT, KC_WWW_BACK, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, KC_CALC, KC_TRNS,
+    C(KC_A), C(KC_Z), C(KC_X), C(KC_C), C(KC_V), C(KC_Y), KC_DEL, KC_MPRV, KC_MEDIA_PLAY_PAUSE, KC_MNXT, KC_MAIL, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
   /*
   * Layer _FN2_LAYER
   * ,-----------------------------------------------------------------------------------------.
-  * | USB | BT1 | BT2 | BT3 | BT4 |BTDEL|  F6 |  F7 | F8 | LEDOF|LEDON| LEDI | LEDV | LAYER_0|
+  * |     |     |     |     |     |    |      |      |     |     |    |      |      |         |
   * |-----------------------------------------------------------------------------------------+
-  * | Tab    |  q  | UP  |  e  |  r  |  t  |  y  |  u  |  i  |  o  | PS | HOME | END |   \    |
+  * | Tab    |     |     |     |     |     |     | wh_u | wh_l | ms_u | wh_r |   |     |      |
   * |-----------------------------------------------------------------------------------------+
-  * | Esc     |LEFT |DOWN |RIGHT|  f  |  g  |  h  |  j  |  k  |  l  | PGUP|PGDN |    Enter    |
+  * |    |     |r clck |m clck| l clck |  | WH_U |  ms_left  |  ms_d  |  ms_r  |  |  Enter    |
   * |-----------------------------------------------------------------------------------------+
-  * | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
+  * | Shift      |     |     |     |     |     |     |     |  ,  |INSRT| DEL |    Shift       |
   * |-----------------------------------------------------------------------------------------+
-  * | Ctrl  |  L1   |  Alt  |               space             |  Alt  |  FN1  |  FN2  | Ctrl  |
+  * | Ctrl  |  L1   |  Alt  |               l clck            |  Alt  |TG(FN2)|TG(FN3)|TG(FN4)|
   * \-----------------------------------------------------------------------------------------/
   *
   */
  [_FN2_LAYER] = KEYMAP( /* Base */
-    KC_AP2_USB, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_AP2_BT_UNPAIR, KC_TRNS, KC_TRNS, KC_TRNS, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED, TO(_BASE_LAYER),
-    KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_HOME, KC_END, KC_TRNS,
-    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(_BASE_LAYER),
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_WH_UP, KC_MS_WH_LEFT, KC_MS_UP, KC_MS_WH_RIGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_LEFT, KC_MS_BTN2, KC_MS_BTN3, KC_MS_BTN1, KC_TRNS, KC_MS_WH_DOWN, KC_MS_WH_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_TRNS, KC_PGDN, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_DEL, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_BTN1, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
  ),
  /*
 * Layer _FN3_LAYER
@@ -100,36 +104,37 @@ enum anne_pro_layers {
 * |-----------------------------------------------------------------------------------------+
 * | TG(FN1)|     |     |     |     |     |     |     |     |     |     |     |             |
 * |-----------------------------------------------------------------------------------------+
-* |            |     |     |     |     |     |     |     |     |     |     |                |
+* |   shift  |UNDO | CUT| COPY | PASTE | REDO|     |     |     |     |     |                |
 * |-----------------------------------------------------------------------------------------+
 * |       |       |       |                                 |       |TG(FN2)|TG(FN3)|TG(FN4)|
 * \-----------------------------------------------------------------------------------------/
 */
 [_FN3_LAYER] = KEYMAP( /* Base */
-    KC_TRNS, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED, TO(_BASE_LAYER),
-    MO(_FN2_LAYER), KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_HOME, KC_END, KC_TRNS,
-    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_DEL, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NUMLOCK, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS, KC_TRNS, KC_TRNS, TO(_BASE_LAYER),
+    KC_TAB, KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_KP_7 KC_KP_8, KC_KP_9, KC_KP_PLUS, KC_HOME, KC_END, KC_TRNS,
+    KC_TRNS, KC_LCTL, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_PLUS, KC_BSPC, KC_KP_ENTER,
+    KC_LSFT, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), C(KC_Y), KC_DEL, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_ENTER, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_KP_0, KC_KP_DOT, KC_TRNS, KC_TRNS, KC_TRNS
  ),
-  /*
-* Layer _FN4_LAYER
-* ,-----------------------------------------------------------------------------------------.
-* |     |     |     |     |     |     |     |     |     |     |     |     |     |           |
-* |-----------------------------------------------------------------------------------------+
-* |        |     |     |     |     |     |     |     |     |     |     |     |     |        |
-* |-----------------------------------------------------------------------------------------+
-* | TG(FN1)|     |     |     |     |     |     |     |     |     |     |     |             |
-* |-----------------------------------------------------------------------------------------+
-* |            |     |     |     |     |     |     |     |     |     |     |                |
-* |-----------------------------------------------------------------------------------------+
-* |       |       |       |                                 |       |TG(FN2)|TG(FN3)|TG(FN4)|
-* \-----------------------------------------------------------------------------------------/
-*/
-[_FN4_LAYER] = KEYMAP( /* Base */
-    KC_TRNS, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED,  KC_TRNS,
-    MO(_FN2_LAYER), KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_HOME, KC_END, KC_TRNS,
-    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS,
+/*
+  * Layer _FN4_LAYER
+  * ,-----------------------------------------------------------------------------------------.
+  * | USB | BT1 | BT2 | BT3 | BT4 |BTDEL|  F6 |  F7 | F8 | LEDOF|LEDON| LEDI | LEDV | LAYER_0|
+  * |-----------------------------------------------------------------------------------------+
+  * | Tab    |     |     |  UP |  r  |  t  |  y  |  u  |  i  |  o  | PS | HOME | END |   \    |
+  * |-----------------------------------------------------------------------------------------+
+  * |       |  Ctrl |LEFT |DOWN |RIGHT|  g  |  h  |  j  |  k  |  l  | PGUP|PGDN |    Enter    |
+  * |-----------------------------------------------------------------------------------------+
+  * | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
+  * |-----------------------------------------------------------------------------------------+
+  * | Ctrl  |  L1   |  Alt  |               space             |  Alt  |TG(FN2)|TG(FN3)|TG(FN4)|
+  * \-----------------------------------------------------------------------------------------/
+  *
+  */
+ [_FN4_LAYER] = KEYMAP( /* Base */
+    KC_AP2_USB, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_AP2_BT_UNPAIR, KC_TRNS, KC_TRNS, KC_TRNS, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED, TO(_BASE_LAYER),
+    KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_HOME, KC_END, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_DEL, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
  ),
@@ -180,7 +185,7 @@ layer_state_t layer_state_set_user(layer_state_t layer) {
       break;
     case _FN3_LAYER:
       // Set the leds to cyan
-      annepro2LedSetForegroundColor(0x00, 0xFF, 0xFF);
+      annepro2LedSetForegroundColor(0x1F, 0x73, 0x6A);
       break;
     case _FN4_LAYER:
       // Set the leds to yellow
@@ -223,4 +228,36 @@ bool led_update_user(led_t leds) {
   }
 
   return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	switch (keycode) { //For keycode overrides
+		case ATABF:	//Alt tab forwards
+		  if (record->event.pressed) {
+			if (!is_alt_tab_active) {
+			  is_alt_tab_active = true;
+			  register_code(KC_LALT);
+			}
+			alt_tab_timer = timer_read();
+			register_code(KC_TAB);
+		  } else {
+			unregister_code(KC_TAB);
+		  }
+		  return true;
+		case ATABR:	//Alt tab reverse
+		  if (record->event.pressed) {
+			if (!is_alt_tab_active) {
+			  is_alt_tab_active = true;
+			  register_code(KC_LALT);
+			}
+			alt_tab_timer = timer_read();
+			register_code(KC_LSHIFT);
+			register_code(KC_TAB);
+		  } else {
+			unregister_code(KC_TAB);
+            unregister_code(KC_LSHIFT);
+		  }
+		  return true;
+	}
+	return true;
 }
